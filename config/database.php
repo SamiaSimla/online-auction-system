25efcbc4f2eb9db742b1b9c6c05d1466b4e564a8
simla <samiasimla819@gmail.com>
2026-05-18 21:33:02 +0600

    added database, functions, sellerController page

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+$dbname = 'online_auction_system';
+
+$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
+
+if(!$conn){
+    die('Database connection failed: '.mysqli_connect_error());
+}
+?>
