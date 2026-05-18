151630784e30cb3e839f7c3586a9f7ce97ed9c43
simla <samiasimla819@gmail.com>
2026-05-18 21:43:20 +0600

    fix bug related to database connection

--- models/userModel.php
+++ models/userModel.php
@@ -1,5 +1,5 @@
 <?php
-require_once(__DIR__.'/../config/db.php');
+require_once(__DIR__.'/../config/database.php');
 
 function registerUser($name, $email, $phone, $bio, $password){
     global $conn;
